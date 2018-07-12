2c63b461949816c690114b46fa9064cd35f01c2d
andrewmcnutt <wanderbumble@gmail.com>
2018-07-12 06:29:14 -0700

    Added some instructions.

--- src/components/PokerTable/index.js
+++ src/components/PokerTable/index.js
@@ -15,6 +15,10 @@ class PokerTable extends Component {
         return (
             <div>
                 <h1>Playing poker!</h1>
+                <div>When a cards border turns blue after clicking on it the card
+                     is set to be discarded when the user clicks the Go button.
+                     To change it back simply click the card again.
+                </div>
                 <DealBtn />
                 <GoBtn />
                 <ScoreBoard />
